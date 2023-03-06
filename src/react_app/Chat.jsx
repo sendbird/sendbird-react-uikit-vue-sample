11dc7cfeee7f8e20049478916b3741a40e744d58
Tyler Hammer <tyler.hammer@sendbird.com>
2023-03-06 15:12:53 +0900

    Update example to use useSendbirdStateContext

--- src/react_app/Chat.jsx
+++ src/react_app/Chat.jsx
@@ -1,29 +1,48 @@
-import SendbirdApp from "@sendbird/uikit-react/App";
-import "@sendbird/uikit-react/dist/index.css";
-import withSendbird from "@sendbird/uikit-react/withSendbird";
-import sendbirdSelectors from "@sendbird/uikit-react/sendbirdSelectors";
-
-
-
+import Channel from '@sendbird/uikit-react/Channel';
+import ChannelList from '@sendbird/uikit-react/ChannelList';
+import '@sendbird/uikit-react/dist/index.css';
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
+import sendbirdSelectors from '@sendbird/uikit-react/sendbirdSelectors';
+import useSendbirdStateContext from '@sendbird/uikit-react/useSendbirdStateContext';
+import { useState } from 'react';
 
 const Chat = (props) => {
-    const sdk = sendbirdSelectors.getSdk(props);
+  const [channelUrl, setChannelUrl] = useState(null);
+  const context = useSendbirdStateContext();
+  const sdkInstance = sendbirdSelectors.getSdk(context);
 
-    const currentUser = sdk && sdk.currentUser;
-    if (currentUser) {
+  const currentUser = sdkInstance && sdkInstance.currentUser;
+  if (currentUser) {
+    const getTotalUnreadMessageCount = async () => {
+      const unreadMessageCount = await sdkInstance.groupChannel.getTotalUnreadMessageCount();
+      props.setUnreadMessageCount(unreadMessageCount);
+    };
+    getTotalUnreadMessageCount();
+    props.setSbUserInfo(currentUser);
+  }
 
-        const getTotalUnreadMessageCount = async () => {
-            const unreadMessageCount = await sdk.groupChannel.getTotalUnreadMessageCount();
-            props.setUnreadMessageCount(unreadMessageCount);
-        }
-        getTotalUnreadMessageCount();
-        props.setSbUserInfo(currentUser);
-    }
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ height: '100dvh', width: '100%' }}>
+        <ChannelList
+          onChannelSelect={(channel) => {
+            setChannelUrl(channel.url);
+          }}
+        />
+      </div>
+      <div style={{ height: '100dvh', width: '100%' }}>
+        <Channel channelUrl={channelUrl} />
+      </div>
+    </div>
+  );
+};
 
-    return (<SendbirdApp appId={props.config.APP_ID} userId={props.config.USER_ID} />);
+const Provider = (props) => {
+  return (
+    <SendbirdProvider appId={props.config.APP_ID} userId={props.config.USER_ID} nickname={props.config.NICKNAME}>
+      <Chat {...props} />
+    </SendbirdProvider>
+  );
+};
 
-}
-
-const ChatWithSendbird = withSendbird(Chat)
-
-export default ChatWithSendbird;
+export default Provider;
